7e332e5fec9a205420a22fb904bc562ea62cd858
gangadhara <gangadhara@bootminds.com>
2020-01-15 11:38:55 +0530

    register and logout component created

--- src/app/models/auth/auth.module.ts
+++ src/app/models/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent, LogoutComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
